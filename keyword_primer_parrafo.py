04b071243590ddf2c25a9d815ec92703febaa982
enderjp <enderjtaler@gmail.com>
2023-01-11 21:47:38 -0400

    commit

--- keyword_primer_parrafo.py
+++ keyword_primer_parrafo.py
@@ -87,13 +87,13 @@ def kw_prim_p(soup,keyword):
                         if ( keyword.lower() in parrafo_sin_acentos[cont].lower()) or (keyword_2.lower() in parrafo_sin_acentos[cont].lower() ): 
                                
                               # expresión regular para determinar si en el primer párrafo está la keyword subrayada
-                              pattern = re.compile(r'(?:<(?:span|strong|b)[^>]*>){1,2}'+keyword.lower()+'(?:</(?:span|strong|b)>){1,2}|(?:<(?:em|strong)[^>]*>){2}'+keyword.lower()+'(?:</(?:em|strong)>){2}',re.IGNORECASE)  
-                              pattern2 = re.compile(r'(?:<(?:span|strong|b)[^>]*>){1,2}'+keyword_2.lower()+'(?:</(?:span|strong|b)>){1,2}|(?:<(?:em|strong)[^>]*>){2}'+keyword_2.lower()+'(?:</(?:em|strong)>){2}',re.IGNORECASE)  
+                            #  pattern = re.compile(r'(?:<(?:span|strong|b)[^>]*>){1,2}'+keyword.lower()+'(?:</(?:span|strong|b)>){1,2}|(?:<(?:em|strong)[^>]*>){2}'+keyword.lower()+'(?:</(?:em|strong)>){2}',re.IGNORECASE)  
+                            #  pattern2 = re.compile(r'(?:<(?:span|strong|b)[^>]*>){1,2}'+keyword_2.lower()+'(?:</(?:span|strong|b)>){1,2}|(?:<(?:em|strong)[^>]*>){2}'+keyword_2.lower()+'(?:</(?:em|strong)>){2}',re.IGNORECASE)  
                             
-                           #if (soup.find_all(["p","strong","b","span"],string=re.compile('^{0}$'.format(quitar_acentos(keyword)),flags=re.IGNORECASE), recursive=True) or soup.find_all(["p","strong","b","span"],string=re.compile('^{0}$'.format(keyword),flags=re.IGNORECASE), recursive=True) ):      
-                              if (pattern.search(str(parrafo_sin_acentos[cont].lower())) or pattern2.search(str(parrafo_sin_acentos[cont].lower())) ):
-                                  return "SI", "SI"
-                              else:
+                           if (soup.find_all(["p","strong","b","span"],string=re.compile('^{0}$'.format(quitar_acentos(keyword)),flags=re.IGNORECASE), recursive=True) or soup.find_all(["p","strong","b","span"],string=re.compile('^{0}$'.format(keyword),flags=re.IGNORECASE), recursive=True) ):      
+                             # if (pattern.search(str(parrafo_sin_acentos[cont].lower())) or pattern2.search(str(parrafo_sin_acentos[cont].lower())) ):
+                              return "SI", "SI"
+                           else:
                                
                                return "NO", "SI" 
                     
@@ -103,13 +103,13 @@ def kw_prim_p(soup,keyword):
             # de no ser así y arroja error al verificar fecha, analizar igual si la keyword está alli
             except:
                         if ( keyword.lower() in parrafo_sin_acentos[cont].lower()) or (keyword_2.lower() in parrafo_sin_acentos[cont].lower() ): 
-                             pattern = re.compile(r'(?:<(?:span|strong|b)[^>]*>){1,2}'+keyword.lower()+'(?:</(?:span|strong|b)>){1,2}|(?:<(?:em|strong)[^>]*>){2}'+keyword.lower()+'(?:</(?:em|strong)>){2}',re.IGNORECASE)  
-                             pattern2 = re.compile(r'(?:<(?:span|strong|b)[^>]*>){1,2}'+keyword_2.lower()+'(?:</(?:span|strong|b)>){1,2}|(?:<(?:em|strong)[^>]*>){2}'+keyword_2.lower()+'(?:</(?:em|strong)>){2}',re.IGNORECASE)  
-                             print(" Parrafo: ", parrafo_sin_acentos[cont].lower())
-                          #if (soup.find_all(["p","strong","b","span"],string=re.compile('^{0}$'.format(quitar_acentos(keyword)),flags=re.IGNORECASE), recursive=True) or soup.find_all(["p","strong","b","span"],string=re.compile('^{0}$'.format(keyword),flags=re.IGNORECASE), recursive=True) ):      
-                             if (pattern.search(str(parrafo_sin_acentos[cont].lower())) or pattern2.search(str(parrafo_sin_acentos[cont].lower())) ):
+                           #  pattern = re.compile(r'(?:<(?:span|strong|b)[^>]*>){1,2}'+keyword.lower()+'(?:</(?:span|strong|b)>){1,2}|(?:<(?:em|strong)[^>]*>){2}'+keyword.lower()+'(?:</(?:em|strong)>){2}',re.IGNORECASE)  
+                            # pattern2 = re.compile(r'(?:<(?:span|strong|b)[^>]*>){1,2}'+keyword_2.lower()+'(?:</(?:span|strong|b)>){1,2}|(?:<(?:em|strong)[^>]*>){2}'+keyword_2.lower()+'(?:</(?:em|strong)>){2}',re.IGNORECASE)  
+                            # print(" Parrafo: ", parrafo_sin_acentos[cont].lower())
+                          if (soup.find_all(["p","strong","b","span"],string=re.compile('^{0}$'.format(quitar_acentos(keyword)),flags=re.IGNORECASE), recursive=True) or soup.find_all(["p","strong","b","span"],string=re.compile('^{0}$'.format(keyword),flags=re.IGNORECASE), recursive=True) ):      
+                           ##  if (pattern.search(str(parrafo_sin_acentos[cont].lower())) or pattern2.search(str(parrafo_sin_acentos[cont].lower())) ):
                                  return "SI", "SI"
-                             else:
+                          else:
                                 return "NO","SI"
                         
                       #  elif ( keyword.lower() in parrafos[1].get_text().lower()) or (keyword_2.lower() in parrafos[1].get_text().lower() ): 
